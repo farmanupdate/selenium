8a4caafbdbc7937ba8cfd1ecd625e595db4428e5
ZainAaliya <farmanupdate@gmail.com>
2020-04-26 17:10:43 +0530

    My GIT integration project

---
+++ GITDemo/src/test/java/gitDemoPackage/GITDemoClass.java
@@ -0,0 +1,11 @@
+package gitDemoPackage;
+
+import org.testng.annotations.Test;
+
+public class GITDemoClass {
+  @Test
+  public void f() {
+	  
+	  System.out.println("This is for GITHUB Test");
+  }
+}
